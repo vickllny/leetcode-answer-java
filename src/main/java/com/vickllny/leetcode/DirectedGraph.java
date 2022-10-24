d8e2a9235e13e8abfa46e84ab3535b4e1b66d0bf
vickllny <vickllny@163.com>
2022-10-25 00:04:57 +0800

    findWhetherExistsPath

---
+++ src/main/java/com/vickllny/leetcode/DirectedGraph.java
@@ -0,0 +1,19 @@
+package com.vickllny.leetcode;
+
+import java.util.*;
+
+/**
+ * @description:
+ * @author: vickllny
+ * @date 2022-10-23 20:48
+ * @leetcode: https://leetcode.cn/problems/route-between-nodes-lcci/submissions/
+ */
+public class DirectedGraph {
+
+    public boolean findWhetherExistsPath(int n, int[][] graph, int start, int target) {
+
+        return false;
+    }
+
+
+}
