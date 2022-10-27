9464ad1df0fa2bd8db1fcab2691689b01c0c7d67
vickllny <vickllny@163.com>
2022-10-27 10:49:10 +0800

    bst

---
+++ src/main/java/com/vickllny/leetcode/LowestBinarySearchTree.java
@@ -0,0 +1,24 @@
+package com.vickllny.leetcode;
+
+/**
+ * @description: 最小二叉搜索树
+ * @author: vickllny
+ * @date 2022-10-27 10:46
+ * @leetcode: https://leetcode.cn/problems/minimum-height-tree-lcci/
+ */
+public class LowestBinarySearchTree {
+
+    public TreeNode sortedArrayToBST(int[] nums) {
+
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
