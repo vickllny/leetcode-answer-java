f8b15bc2256198378d62325ff159f6d3c22f8407
vickllny <vickllny@163.com>
2024-08-27 22:16:57 +0800

    TODO legal-binary-search-tree-lcci

--- src/main/java/com/vickllny/leetcode/CheckBalanceLcci.java
+++ src/main/java/com/vickllny/leetcode/CheckBalanceLcci.java
@@ -11,7 +11,17 @@ import com.vickllny.common.TreeNode;
 public class CheckBalanceLcci {
 
     public boolean isBalanced(TreeNode root) {
+        if(root == null){
+            return true;
+        }
+        return Math.abs(height(root.left) - height(root.right)) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+    }
 
+    int height(TreeNode node){
+        if(node == null){
+            return 0;
+        }
+        return Math.max(height(node.left), height(node.right)) + 1;
     }
 
 }
---
+++ src/main/java/com/vickllny/leetcode/LegalBinarySearchTreeLcci.java
@@ -0,0 +1,17 @@
+package com.vickllny.leetcode;
+
+import com.vickllny.common.TreeNode;
+
+/**
+ * @description:
+ * @author: vickllny
+ * @date 2024-08-27 23:32
+ * @leetcode: https://leetcode.cn/problems/legal-binary-search-tree-lcci/description/
+ */
+public class LegalBinarySearchTreeLcci {
+
+    public boolean isValidBST(TreeNode root) {
+
+    }
+
+}
