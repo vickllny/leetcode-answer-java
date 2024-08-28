5f109677676c2d701f467640f3810b5c17edc5a9
vickllny <vickllny@163.com>
2024-08-29 00:20:22 +0800

    todo successor-lcci

--- src/main/java/com/vickllny/leetcode/LegalBinarySearchTreeLcci.java
+++ src/main/java/com/vickllny/leetcode/LegalBinarySearchTreeLcci.java
@@ -2,6 +2,10 @@ package com.vickllny.leetcode;
 
 import com.vickllny.common.TreeNode;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @description:
  * @author: vickllny
@@ -11,7 +15,40 @@ import com.vickllny.common.TreeNode;
 public class LegalBinarySearchTreeLcci {
 
     public boolean isValidBST(TreeNode root) {
+        if(root == null){
+            return true;
+        }
+        final List<Integer> list = new ArrayList<>();
 
+        return validBST(root, list);
     }
 
+    boolean validBST(final TreeNode root, final List<Integer> list){
+        if(root == null){
+            return true;
+        }
+        if(!validBST(root.left, list)){
+            return false;
+        }
+        if(list.isEmpty()){
+            list.add(root.val);
+        }else {
+            if (list.get(list.size() - 1) >= root.val) {
+                return false;
+            }
+            list.add(root.val);
+        }
+        return validBST(root.right, list);
+    }
+
+
+    public static void main(String[] args) {
+
+        final LegalBinarySearchTreeLcci lcci = new LegalBinarySearchTreeLcci();
+
+        final TreeNode root = new TreeNode(1);
+        root.left = null;
+        root.right = new TreeNode(1);
+        System.out.println(lcci.isValidBST(root));
+    }
 }
---
+++ src/main/java/com/vickllny/leetcode/SuccessorLcci.java
@@ -0,0 +1,17 @@
+package com.vickllny.leetcode;
+
+import com.vickllny.common.TreeNode;
+
+/**
+ * @description:
+ * @author: vickllny
+ * @date 2024-08-29 00:19
+ * @leetcode: https://leetcode.cn/problems/successor-lcci/description/
+ */
+public class SuccessorLcci {
+
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+
+    }
+
+}
